c49c1cb7c7d79dbfe9fe9433422d2eb93907c95c
Joao Lourenco <joao.lourenco@growthengineering.co.uk>
2025-10-29 12:31:09 +0000

    open a new iab instance if pdf is detected, and download for the other type files

--- src/ios/CDVWKInAppBrowser.m
+++ src/ios/CDVWKInAppBrowser.m
@@ -181,6 +181,18 @@ static CDVWKInAppBrowser* instance = nil;
     return @"Request failed";
 }
 
+- (BOOL) isPdfFile:(NSURL*)url
+{
+    if (!url) {
+        return NO;
+    }
+    NSString* urlString = [url absoluteString];
+    NSString* lowerUrl = [urlString lowercaseString];
+    return [lowerUrl hasSuffix:@".pdf"] || 
+           [lowerUrl containsString:@".pdf?"] || 
+           [lowerUrl containsString:@".pdf#"];
+}
+
 - (BOOL) isDownloadableFile:(NSURL*)url
 {
     NSString* urlString = [url absoluteString];
@@ -350,6 +362,26 @@ static CDVWKInAppBrowser* instance = nil;
         }];
     
     [downloadTask resume];
+}
+
+- (void)openDownloadUrlInNewWindow:(NSURL*)url
+{
+    NSLog(@"Opening download URL in new InAppBrowser window: %@", [url absoluteString]);
+    
+    // Create a new InAppBrowser instance for the download URL
+    CDVWKInAppBrowser* newBrowser = [[CDVWKInAppBrowser alloc] init];
+    
+    // Initialize the new browser with the same settings as the current one
+    [newBrowser pluginInitialize];
+    
+    // Set up options for the new browser (with close button)
+    NSString* options = @"location=yes,toolbar=yes,zoom=yes,hardwareback=yes";
+    
+    // Create a command to open the new browser
+    CDVInvokedUrlCommand* command = [[CDVInvokedUrlCommand alloc] initWithArguments:@[[url absoluteString], @"_blank", options] callbackId:@"InAppBrowserDownload" className:@"InAppBrowser" methodName:@"open"];
+    
+    // Open the new browser window
+    [newBrowser open:command];
 }
 
 - (void)open:(CDVInvokedUrlCommand*)command
@@ -787,7 +819,15 @@ static CDVWKInAppBrowser* instance = nil;
     // Check if this is a downloadable file (only for link clicks, not page loads)
     if (navigationAction.navigationType == WKNavigationTypeLinkActivated && [self isDownloadableFile:url]) {
         NSLog(@"Detected download link click: %@", [url absoluteString]);
-        [self handleDownload:url];
+        
+        // If it's a PDF, open in new window; otherwise download
+        if ([self isPdfFile:url]) {
+            NSLog(@"PDF detected - opening in new InAppBrowser window");
+            [self openDownloadUrlInNewWindow:url];
+        } else {
+            NSLog(@"Non-PDF file detected - downloading");
+            [self handleDownload:url];
+        }
         decisionHandler(WKNavigationActionPolicyCancel);
         return;
     }
